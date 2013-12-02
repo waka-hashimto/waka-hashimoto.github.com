ba464cf15f5bad15523b68c05dff34fa564eba65
jollyjoester <jollyjoester@gmail.com>
2013-12-02 10:49:48 +0900

    adjust color

--- js/setting.js
+++ js/setting.js
@@ -18,19 +18,19 @@ OpenSpending.Styles = OpenSpending.Styles || {};
 OpenSpending.Styles.Cofog = {
 
   /* chigasaki cofog */
-  '1': { icon: 'icons/civilian-action.svg', color: '#138EC0', bcolor: '#935B3B' },
-  '2': { icon: 'icons/publicaffairs.svg', color: '#138EC0', bcolor: '#0AB971' },
-  '3': { icon: 'icons/family.svg', color: '#138EC0', bcolor: '#4E6D00' },
-  '4': { icon: 'icons/health.svg', color: '#138EC0', bcolor: '#D33673' },
-  '5': { icon: 'icons/human-resources.svg', color: '#138EC0', bcolor: '#2A3A03' },
-  '6': { icon: 'icons/forest.svg', color: '#138EC0', bcolor: '#EC2406' },
-  '7': { icon: 'icons/traffic-watersup.svg', color: '#138EC0', bcolor: '#938626' },
-  '8': { icon: 'icons/coal.svg', color: '#138EC0', bcolor: '#C75746' },
-  '9': { icon: 'icons/order-safety.svg', color: '#138EC0', bcolor: '#D33673' },
-  '10': { icon: 'icons/schools.svg', color: '#138EC0', bcolor: '#790586' },
-  '11': { icon: 'icons/economic-aid.svg', color: '#138EC0', bcolor: '#790586' },
-  '12': { icon: 'icons/money.svg', color: '#138EC0', bcolor: '#790586' },
-  '13': { icon: 'icons/financial-admin.svg', color: '#138EC0', bcolor: '#790586' }
+  '1': { icon: 'icons/civilian-action.svg', color: '#138EC0', bcolor: '#138EC0' },
+  '2': { icon: 'icons/publicaffairs.svg', color: '#138EC0', bcolor: '#138EC0' },
+  '3': { icon: 'icons/family.svg', color: '#138EC0', bcolor: '#138EC0' },
+  '4': { icon: 'icons/health.svg', color: '#138EC0', bcolor: '#138EC0' },
+  '5': { icon: 'icons/human-resources.svg', color: '#138EC0', bcolor: '#138EC0' },
+  '6': { icon: 'icons/forest.svg', color: '#138EC0', bcolor: '#138EC0' },
+  '7': { icon: 'icons/traffic-watersup.svg', color: '#138EC0', bcolor: '#138EC0' },
+  '8': { icon: 'icons/coal.svg', color: '#138EC0', bcolor: '#138EC0' },
+  '9': { icon: 'icons/order-safety.svg', color: '#138EC0', bcolor: '#138EC0' },
+  '10': { icon: 'icons/schools.svg', color: '#138EC0', bcolor: '#138EC0' },
+  '11': { icon: 'icons/economic-aid.svg', color: '#138EC0', bcolor: '#138EC0' },
+  '12': { icon: 'icons/money.svg', color: '#138EC0', bcolor: '#138EC0' },
+  '13': { icon: 'icons/financial-admin.svg', color: '#138EC0', bcolor: '#138EC0' }
   
 };
 
