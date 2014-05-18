2be39e0f340cd5c57495f56ce2b8e5ad820a1ea4
jollyjoester <jollyjoester@gmail.com>
2014-05-18 13:30:38 +0900

    Change to 2014.

--- js/setting.js
+++ js/setting.js
@@ -10,8 +10,8 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'chigasaki_2013_budget';
-OpenSpending.year = '2013';
+OpenSpending.identifier = 'chigasaki_2014_budget';
+OpenSpending.year = '2014';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
 
